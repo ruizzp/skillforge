5d4c24c6b546bd2b891700230b694f053ce029f4
fidelisfelipe <atosfiel@gmail.com>
2026-05-08 00:19:40 +0100

    Fix hub RabbitTemplate not using Jackson converter on dispatch
    ProblemPublisher injected the Spring Boot auto-configured RabbitTemplate
    which used SimpleMessageConverter. ProblemMessage is not Serializable,
    so convertAndSend threw MessageConversionException on every dispatch call.
    Added explicit RabbitTemplate bean wired with the Jackson converter.

--- guild-hub/src/main/java/com/skillforge/hub/amqp/AmqpConfig.java
+++ guild-hub/src/main/java/com/skillforge/hub/amqp/AmqpConfig.java
@@ -1,6 +1,8 @@
 package com.skillforge.hub.amqp;
 
 import org.springframework.amqp.core.*;
+import org.springframework.amqp.rabbit.connection.ConnectionFactory;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
 import org.springframework.amqp.support.converter.MessageConverter;
 import org.springframework.beans.factory.annotation.Value;
@@ -59,5 +61,12 @@ public class AmqpConfig {
     @Bean
     public MessageConverter jsonConverter() {
         return new Jackson2JsonMessageConverter();
+    }
+
+    @Bean
+    public RabbitTemplate rabbitTemplate(ConnectionFactory cf, MessageConverter converter) {
+        var template = new RabbitTemplate(cf);
+        template.setMessageConverter(converter);
+        return template;
     }
 }
